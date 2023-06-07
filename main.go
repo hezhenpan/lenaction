c82046c6564ac11f2f20c5fa0dfdb4858b62e732
hezhenpan <hezhenpan@hotmail.com>
2023-06-07 16:37:54 +0800

    Dev awsome proj (#3)
    * add dev version
    
    * add private lib
    
    * change private token

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/hezhenpan/aws-private-lib/awpkg"
+	"time"
+)
+
 func main() {
-	println("hello world")
+	plus := awpkg.GetNBPlus("zhangsan", "lisi")
+	v := time.Now().String()
+	println("my time version", v)
+	println("hello world", plus)
 }
